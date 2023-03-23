8da436d1f78531ae53c11f810232a0ae325a5d4b
Gio Rojas <gio.rojas15@gmail.com>
2023-03-22 23:46:38 -0700

    finished Q3

--- lab/lab09/lab09.py
+++ lab/lab09/lab09.py
@@ -55,14 +55,14 @@ def inc_subseqs(s):
     """
     def subseq_helper(s, prev):
         if not s:
-            return ____________________
+            return [[]]
         elif s[0] < prev:
-            return ____________________
+            return subseq_helper(s[1:], prev)
         else:
-            a = ______________________
-            b = ______________________
-            return insert_into_all(________, ______________) + ________________
-    return subseq_helper(____, ____)
+            a = subseq_helper(s[1:], s[0]) #include
+            b = subseq_helper(s[1:], prev) #exclude
+            return insert_into_all(s[0], a) + b
+    return subseq_helper(s, 0)
 
 
 def num_trees(n):
