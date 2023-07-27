ad88c1bc17e7d0dcb3b03217e66cf9f6c97a1d2b
Gio Rojas <gio.rojas15@gmail.com>
2023-07-26 23:09:10 -0700

    finished lab13

--- lab/lab13/lab13.sql
+++ lab/lab13/lab13.sql
@@ -2,17 +2,21 @@
 
 
 CREATE TABLE average_prices AS
-  SELECT category, average_price FROM products GROUP BY category;
+  SELECT category, AVG(MSRP) AS average_price FROM products GROUP BY category;
 
 
 CREATE TABLE lowest_prices AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT store, item, MIN(price) FROM inventory GROUP BY item;
+
+
+CREATE TABLE deal_ratios AS
+  SELECT name, MIN(MSRP / rating) FROM products GROUP BY category;
 
 
 CREATE TABLE shopping_list AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT bdbc.name, lp.store FROM deal_ratios AS bdbc, lowest_prices AS lp WHERE bdbc.name = lp.item;
 
 
 CREATE TABLE total_bandwidth AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT SUM(s.Mbs) FROM stores AS s, shopping_list AS sl WHERE s.store = sl.store;
 
